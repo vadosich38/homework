394aebf4ae3cd014a208aba8b5a97758ac06c3cf
Vladyslav <vadosich38@gmail.com>
2022-11-21 07:23:39 +0100

    M15.2-task1 https://t.me/c/1614117860/652

---
+++ 15.2/1.py
@@ -0,0 +1,16 @@
+nums_list = []
+N = int(input('Кол-во чисел в списке: '))
+for _ in range(N):
+    num = int(input('Очередное число: '))
+    nums_list.append(num)
+
+maximum = nums_list[0]
+minimum = nums_list[0]
+
+for i in nums_list:
+    if maximum < i:
+        maximum = i
+    if minimum > i:
+        minimum = i
+print('Максимальное число в списке:', maximum)
+print('Минимальное число в списке:', minimum)
