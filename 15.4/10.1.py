1bc5c894914f8962ad638f841635ad859ed350d6
Vladyslav <vadosich38@gmail.com>
2022-11-24 08:05:33 +0100

    M15.5-task10 + 10.1 added (2) https://t.me/c/1614117860/673

---
+++ 15.4/10.1.py
@@ -0,0 +1,21 @@
+count = int(input("Сколько чисел в списке: "))
+num_list = []
+def input_list():
+    for _ in range(count):
+        num = int(input("Введите число: "))
+        num_list.append(num)
+def sort():
+    lenth = len(num_list)
+    min = 10e16
+    for mult in range(lenth):
+        for num in range(0 + mult, lenth):
+            if num_list[num] < min:
+                min = num_list[num]
+                pos = num
+
+        num_list[0 + mult], num_list[pos] = num_list[pos], num_list[0 + mult]
+        min = 10e16
+    print(num_list)
+
+input_list()
+sort()
