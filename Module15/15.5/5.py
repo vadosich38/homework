08d0ca3c67887f25a58fd9267e863985ed0510c5
Vladyslav <vadosich38@gmail.com>
2022-11-27 19:57:47 +0100

    fix directory, fix 15.5 -> task 5

--- Module15/15.5/5.py
+++ Module15/15.5/5.py
@@ -1,21 +1,18 @@
 films_list = ["Крепкий орешек", "Назад в будущее", "Таксист", "Леон", "Богемская рапсодия", "Город грехов", "Мементо", "Отступники", "Деревня"]
 favorite_films_list = []
 search_name_film = ""
-flag = True
 
 while True:
     search_name_film = input("Введите название фильма: ")
     if search_name_film == ".":
         break
-    else:
-        for film_name in films_list:
-            if film_name == search_name_film:
-                flag = False
-                favorite_films_list.append(search_name_film)
-                print("Фильм добавлен в список любимых фильмов!")
-
-        if flag:
-            print("Ошибка! Такой фильм не найден!")
-        flag = True
+    flag = True
+    for film_name in films_list:
+        if film_name == search_name_film:
+            flag = False
+            favorite_films_list.append(search_name_film)
+            print("Фильм добавлен в список любимых фильмов!")
+    if flag:
+        print("Ошибка! Такой фильм не найден!")
 
 print("Список любимых фильмов:", favorite_films_list)
--- Module16/16.4/1.py
+++ Module16/16.4/1.py
@@ -1,1 +1,6 @@
-matrix = [[1, 2 ,3], [4, 5, 6], [7, 8, 9]]
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+for num1 in range(3):
+    for num2 in range(3):
+        print(matrix[num1][num2], end = " ")
+    print()
