44731252dd68394683d26ec235058bedd49f09da
Vladyslav <47224640+vadosich38@users.noreply.github.com>
2023-02-23 08:40:40 +0100

    27.3 1, 2 https://t.me/c/1614117860/1076 https://t.me/c/1614117860/1077

---
+++ Module27/27.3/1.1.py
@@ -0,0 +1,16 @@
+from typing import Callable, Any
+
+
+def decorator(func: Callable) -> Callable:
+    def wrapped_func(*args, **kwargs) -> Any:
+        for _ in range(2):
+            func(*args, **kwargs)
+
+    return wrapped_func
+
+@decorator
+def greeting(name: str) -> None:
+    print('Привет, {name}!'.format(name=name))
+
+
+greeting('Tom')
